109782e412f425365ba5b0c7328cf7e09872a90d
deviprasad742@gmail.com <deviprasad742@gmail.com@0012df05-ec95-5848-58a4-a14b0222cebd>
2013-03-15 08:56:26 +0000

    Displaying score on the status bar

--- source/ScoreNotifierPlugin/src/scorenotifierplugin/ILogger.java
+++ source/ScoreNotifierPlugin/src/scorenotifierplugin/ILogger.java
@@ -3,4 +3,6 @@ package scorenotifierplugin;
 public interface ILogger {
 	void log(String matchURL, String log, String score);
 	void log(String matchURL, String log);
+	void clearStatusBar();
+	void updateStatusBar(String score);
 }
